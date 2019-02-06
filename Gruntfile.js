78bfdddc4d8cce055927ae8b2f94d191e43d0b17
toolstik <tolstik.va@gmail.com>
2019-02-06 21:37:46 +0300

    ts2gas locally

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,34 @@
+var ts2gas = require('ts2gas');
+
 module.exports = function (grunt) {
+
+    var ts2gasConfig = {
+        "target": "es5",
+        "lib": [
+            "es2015",
+            "es5",
+            "es6",
+            "es2017"
+        ],
+        "strict": true,
+        "inlineSourceMap": true,
+    };
+
+    function trinspileTs(tsFile) {
+        var tsBody = grunt.file.read(tsFile);
+        var jsBody = ts2gas(tsBody, ts2gasConfig);
+        var jsFile = tsFile.substr(0, tsFile.lastIndexOf(".")) + ".js";
+        grunt.file.write(jsFile, jsBody);
+        grunt.file.delete(tsFile, jsBody);
+    };
+
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         concat: {
             src: {
                 src: 'src/**/*.ts',
-                dest: 'dist/gtable.ts'
+                dest: 'dist/gibernate.ts'
             },
             spec: {
                 src: ['spec/spec.ts', 'spec/TestSuite.ts', 'spec/**/*.ts'],
@@ -30,8 +54,16 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-exec');
 
-    grunt.registerTask('build:src', ['concat:src']);
-    grunt.registerTask('build:test', ['build:src', 'concat:spec', 'copy:manifest']);
+    grunt.task.registerTask('compile', function (target) {
+        if (target == "src") {
+            trinspileTs("dist/gibernate.ts");
+        } else if (target == "spec") {
+            trinspileTs("dist/spec.ts");
+        }
+    });
+
+    grunt.registerTask('build:src', ['concat:src', 'compile:src']);
+    grunt.registerTask('build:test', ['build:src', 'concat:spec', 'compile:spec', 'copy:manifest']);
 
     grunt.registerTask('exec:clasp_run_test', ['exec:clasp_run_repo_test', 'exec:clasp_run_session_test']);
 
